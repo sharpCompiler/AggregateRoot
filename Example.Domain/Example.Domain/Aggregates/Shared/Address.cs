ac02101661455bb56a40a333c78fbc28d8ef95e3
Burim Hajrizaj <burimhajrizaj@gmail.com>
2024-03-08 11:12:41 -0800

    First check in

--- Example.Domain/Example.Domain/Aggregates/Shared/Address.cs
+++ Example.Domain/Example.Domain/Aggregates/Shared/Address.cs
@@ -1,1 +1,28 @@
-﻿public record Address(string Street, string City, State State, string ZipCode);
+﻿public record Address
+
+{
+	public Address(string street, string city, State state, string zipCode)
+	{
+		if (string.IsNullOrWhiteSpace(street))
+			throw new ArgumentNullException($"{nameof(street)} can not be null");
+
+		if (string.IsNullOrWhiteSpace(city))
+			throw new ArgumentNullException($"{nameof(city)} can not be null");
+
+		if (string.IsNullOrWhiteSpace(state))
+			throw new ArgumentNullException($"{nameof(state)} can not be null");
+
+		if (string.IsNullOrWhiteSpace(zipCode))
+			throw new ArgumentNullException($"{nameof(zipCode)} can not be null");
+
+		Street = street;
+		City = city;
+		State = state;
+		ZipCode = zipCode;
+	}
+
+	public string Street { get; }
+	public string City { get; }
+	public State State { get; }
+	public string ZipCode { get; }
+}
